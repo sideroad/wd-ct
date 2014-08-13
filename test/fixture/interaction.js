7b7b9160138e3ce8c0783706b77c766b6c5af222
sideroad <sideroad.jp@gmail.com>
2014-08-14 00:24:49 +0900

    Improve test coverage

---
+++ test/fixture/interaction.js
@@ -0,0 +1,27 @@
+module.exports = function(wd){
+	'use strict';
+	return {
+		input: {
+			'open': function(url){
+				return this.get(url);
+			},
+			'input text': function(str){
+				return this.waitForElementByCss('#text')
+				           .type(str)
+				           .fireEvents('#text', 'change');
+			},
+			'submit': function(){
+				return this.elementByCss('#submit')
+				           .click();
+			}
+		},
+		assertion: {
+			'should submit text parameter as abcde': function(){
+				return this.eval('window.location.href').should.eventually.equal('http://localhost:8000/index.html?text=abcde');
+			},
+			'should submit text parameter as 12345': function(){
+				return this.eval('window.location.href').should.eventually.equal('http://localhost:8000/index.html?text=12345');
+			}
+		}
+	};
+};
