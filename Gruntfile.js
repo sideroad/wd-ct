cb19d3962abd4c5b15813ceaff9052ef7cc52462
sideroad <sideroad.jp@gmail.com>
2014-08-16 11:07:49 +0900

    Change indent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,44 @@
 'use strict';
 
 module.exports = function(grunt) {
-	grunt.initConfig({
-	  clean: ['capture/*'],
-	  jshint: {
-	  	options: {
-	  		jshintrc: true
-	  	},
-	  	src: ['src/*.js']
-	  },
-	  mochacov: {
-	    options: {
-	      require: [],
-	      timeout: "60000"
-	    },
-	    test: {
-	      options: {
-	      	reporter: 'spec'
-	      },
-	      src: ['test/specs/*.js']
-	    },
-	    coverage: {
-	      options: {
-		    coveralls: {
-              repoToken: process.env.COVERALLS_REPO_TOKEN
-            }
-	      },
-	      src: ['test/specs/*.js']
-	    }
-	  }
-	});
+  grunt.initConfig({
+    clean: ['capture/*'],
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      src: ['src/*.js']
+    },
+    mochacov: {
+      options: {
+        require: [],
+        timeout: "60000"
+      },
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/specs/*.js']
+      },
+      coverage: {
+        options: {
+        coveralls: {
+            repoToken: process.env.COVERALLS_REPO_TOKEN
+          }
+        },
+        src: ['test/specs/*.js']
+      }
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-cov');
+  grunt.registerTask('test', ['clean', 'mochacov:test']);
+  grunt.registerTask('coverage', ['mochacov:coverage']);
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-mocha-cov');
-	grunt.registerTask('test', ['clean', 'mochacov:test']);
-	grunt.registerTask('coverage', ['mochacov:coverage']);
-
-	grunt.registerTask('ci', ['mochacov:test', 'mochacov:coverage']);
-	grunt.registerTask('default', ['clean', 'jshint', 'mochacov:test']);
+  grunt.registerTask('ci', ['mochacov:test', 'mochacov:coverage']);
+  grunt.registerTask('default', ['clean', 'jshint', 'mochacov:test']);
 
 };
 
--- test/fixture/interaction-not-enough.js
+++ test/fixture/interaction-not-enough.js
@@ -1,22 +1,24 @@
 module.exports = function(wd){
-	'use strict';
-	return {
-		input: {
-			'open': function(url){
-				return this.get(url);
-			},
-			'submit': function(){
-				return this.elementByCss('#submit')
-				           .click();
-			}
-		},
-		assertion: {
-			'should submit text parameter as abcde': function(){
-				return this.eval('window.location.href').should.eventually.equal('http://localhost:8000/index.html?text=abcde');
-			},
-			'should submit text parameter as 12345': function(){
-				return this.eval('window.location.href').should.eventually.equal('http://localhost:8000/index.html?text=12345');
-			}
-		}
-	};
+  'use strict';
+  return {
+    input: {
+      'open': function(url){
+        return this.get(url);
+      },
+      'submit': function(){
+        return this.elementByCss('#submit')
+                   .click();
+      }
+    },
+    assertion: {
+      'should submit text parameter as abcde': function(){
+        return this.eval('window.location.href')
+                   .should.eventually.equal('http://localhost:8000/index.html?text=abcde');
+      },
+      'should submit text parameter as 12345': function(){
+        return this.eval('window.location.href')
+                   .should.eventually.equal('http://localhost:8000/index.html?text=12345');
+      }
+    }
+  };
 };
