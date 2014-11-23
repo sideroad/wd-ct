8b8b0e33ad8297298459bf7be8ff58aa19896ee5
sideroad <sideroad.jp@gmail.com>
2014-11-23 22:15:53 +0900

    Fixed logging bug

--- test/helpers/setup-site.js
+++ test/helpers/setup-site.js
@@ -2,14 +2,16 @@ module.exports = function(){
     var express = require('express'),
       http = require('http'),
       app = express(),
-      server;
+      server,
+      port = 8000;
 
-  server = http.createServer(app).listen(8000);
+  server = http.createServer(app).listen(port);
   server.on('error', function(err){
     if(err.code !== 'EADDRINUSE') {
       throw err;
     }
   });
+  
   app.use(express.static(__dirname + "/../fixture/site")); //use static files in ROOT/public folder
 	return server;
 };
